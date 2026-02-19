ee38f9dbca0441a26001d2e69ec9297c85cc1348
vanam <vanam@users.noreply.github.com>
2026-02-19 15:23:52 +0000

    deploy: 63e24694bc6c8e7787b4680e0819f1a4e533be8e

---
+++ assets/usePageTitle-B5wq1XKa.js
@@ -0,0 +1,1 @@
+import{r as s}from"./react-vendor-Bgs-6sV-.js";import{u as n}from"./index-BFCo89aA.js";function u(t){const{t:o}=n();s.useEffect(()=>{const r=o("nav.title");document.title=t?`${t} | ${r}`:r},[t,o])}export{u};
